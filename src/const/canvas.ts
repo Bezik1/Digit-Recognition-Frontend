e1838bf42b4f07fe5334e2af8282d964a3ac9d6c
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-08-01 13:33:07 +0200

    com

--- src/const/canvas.ts
+++ src/const/canvas.ts
@@ -1,5 +1,5 @@
 export const GRID_SIZE = 28;
 
-const baseSize = Math.min(window.innerWidth * 0.55, window.innerHeight * 0.7);
+const baseSize = Math.min(window.innerWidth * 0.85, window.innerHeight * 0.7);
 export const PIXEL_SIZE = Math.floor(baseSize / GRID_SIZE);
 export const CANVAS_SIZE = GRID_SIZE * PIXEL_SIZE;
