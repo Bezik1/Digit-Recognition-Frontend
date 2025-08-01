1b86003915ef11e5bfbb48980003e7974de21b85
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-08-01 12:24:47 +0200

    Fixing Pixel Placing Error

--- src/const/canvas.ts
+++ src/const/canvas.ts
@@ -1,3 +1,3 @@
 export const GRID_SIZE = 28;
-export const PIXEL_SIZE = window.innerWidth * 0.015;
+export const PIXEL_SIZE = Math.round(window.innerWidth * 0.015);
 export const CANVAS_SIZE = GRID_SIZE * PIXEL_SIZE;
