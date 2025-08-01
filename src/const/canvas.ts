53fd2df19051d4bc5d42b138d5e0a783bfe64c40
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-08-01 13:25:56 +0200

    Fixing Responsive Design v3

--- src/const/canvas.ts
+++ src/const/canvas.ts
@@ -1,5 +1,5 @@
-export const PIXEL_SIZE =  window.innerWidth >= 1480
-    ? Math.round(window.innerWidth * 0.015)
-    : Math.round(window.innerWidth * 0.029)
-export const GRID_SIZE = PIXEL_SIZE;
+export const GRID_SIZE = 28;
+
+const baseSize = Math.min(window.innerWidth * 0.55, window.innerHeight * 0.7);
+export const PIXEL_SIZE = Math.floor(baseSize / GRID_SIZE);
 export const CANVAS_SIZE = GRID_SIZE * PIXEL_SIZE;
